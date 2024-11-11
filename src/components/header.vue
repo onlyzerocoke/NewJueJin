ef4ff755a2da688586eb2765ac1daa97ea888ec9
lm <18806678143@163.com>
2024-11-11 23:48:08 +0800

    完成小于1190(1260)px，表单边长

--- src/components/header.vue
+++ src/components/header.vue
@@ -355,6 +355,8 @@ let throttle = (func: any, delay: number) => {
 // 应用节流机制到鼠标事件
 const mouseInZhong = throttle(() => {
   zhongHover.value = true;
+  // console.log("zhongHover.value ",zhongHover.value );
+  
 }, 200);
 
 const mouseLeaveZhong = throttle(() => {
@@ -752,6 +754,17 @@ body {
 
     
   }
+
+  @media screen and (max-width: 1260px) {
+    .searchContianer {
+      width: 20rem !important;
+      margin-left:-1rem;
+      
+    }
+
+
+
+}
 
   @media screen and (max-width:1490px)
   {
